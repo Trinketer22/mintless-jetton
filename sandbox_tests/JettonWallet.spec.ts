78a5a2e0a5cfdc71180af251a50bea48db4db219
Trinketer22 <trinketer22@localhost>
2025-08-16 20:20:58 +0300

    Async executor

--- sandbox_tests/JettonWallet.spec.ts
+++ sandbox_tests/JettonWallet.spec.ts
@@ -1455,7 +1455,7 @@ describe('JettonWallet', () => {
             const minterSmc = await blockchain.getContract(jettonMinter.address);
 
             // Sending message but only processing first step of tx chain
-            let res = minterSmc.receiveMessage(internal({
+            let res = await minterSmc.receiveMessage(internal({
                 from: deployer.address,
                 to: jettonMinter.address,
                 body: mintMsg,
@@ -1467,7 +1467,7 @@ describe('JettonWallet', () => {
             expect(outMsgSc.preloadUint(32)).toEqual(Op.internal_transfer);
             expect(await jettonMinter.getTotalSupply()).toEqual(supplyBefore + mintAmount);
 
-            minterSmc.receiveMessage(internal({
+            await minterSmc.receiveMessage(internal({
                 from: deployerJettonWallet.address,
                 to: jettonMinter.address,
                 bounced: true,
@@ -1487,7 +1487,7 @@ describe('JettonWallet', () => {
 
             const walletSmc = await blockchain.getContract(deployerJettonWallet.address);
 
-            const res = walletSmc.receiveMessage(internal({
+            const res = await walletSmc.receiveMessage(internal({
                 from: deployer.address,
                 to: deployerJettonWallet.address,
                 body: transferMsg,
@@ -1501,7 +1501,7 @@ describe('JettonWallet', () => {
 
             expect(await deployerJettonWallet.getJettonBalance()).toEqual(balanceBefore - txAmount);
 
-            walletSmc.receiveMessage(internal({
+            await walletSmc.receiveMessage(internal({
                 from: notDeployerJettonWallet.address,
                 to: walletSmc.address,
                 bounced: true,
@@ -1521,7 +1521,7 @@ describe('JettonWallet', () => {
 
             const walletSmc = await blockchain.getContract(deployerJettonWallet.address);
 
-            const res = walletSmc.receiveMessage(internal({
+            const res = await walletSmc.receiveMessage(internal({
                 from: deployer.address,
                 to: deployerJettonWallet.address,
                 body: burnMsg,
@@ -1535,7 +1535,7 @@ describe('JettonWallet', () => {
 
             expect(await deployerJettonWallet.getJettonBalance()).toEqual(balanceBefore - burnAmount);
 
-            walletSmc.receiveMessage(internal({
+            await walletSmc.receiveMessage(internal({
                 from: jettonMinter.address,
                 to: walletSmc.address,
                 bounced: true,
